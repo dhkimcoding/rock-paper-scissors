e887d5a04b62da33555a1121a39b9167dd804241
dhkimcoding <dhkimcoding@gmail.com>
2025-02-16 16:11:22 +0900

    Add UI to page

--- javascript.js
+++ javascript.js
@@ -23,35 +23,56 @@ function getHumanChoice() {
 
 function playRound(humanChoice, computerChoice) {
     let msg;
-    let humanWin = true;
+    let humanWin = false;
+    let computerWin = false;
     if (humanChoice === computerChoice) {
         msg = `Draw! Both are ${humanChoice}.`;
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
         msg = `Win! ${humanChoice} wins over ${computerChoice}.`;
+        humanWin = true;
     } else if (humanChoice === "paper" && computerChoice === "rock") {
         msg = `Win! ${humanChoice} wins over ${computerChoice}.`;
+        humanWin = true;
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
         msg = `Win! ${humanChoice} wins over ${computerChoice}.`;
+        humanWin = true;
     } else {
         msg = `Lose! ${humanChoice} lose to ${computerChoice}.`;
-        humanWin = false;
+        computerWin = true;
     }
-    console.log(msg);
+    // console.log(msg);
+    result.textContent = msg;
     if (humanWin) humanScore++;
-    else computerScore++;
+    if (computerWin) computerScore++;
+    score.textContent = `Human: ${humanScore}, Computer: ${computerScore}`;
+
 }
 
-function playGame() {
-    let humanChoice;
+function playGame(humanChoice) {
     let computerChoice;
-    for (let i = 0; i < 5; i++) {
-        humanChoice = getHumanChoice();
-        computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
+
+    computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+
+    if (humanScore === 5) {
+        score.textContent += "\nHuman Win!!!!";
+    } 
+    if (computerScore === 5) {
+        score.textContent += "\nComputer Win!!!";
     }
+
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
-playGame();
+// playGame();
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const result = document.querySelector("#result")
+const score = document.querySelector("#score");
+
+rockBtn.addEventListener("click", () => { playGame("rock"); });
+paperBtn.addEventListener("click", () => { playGame("paper"); });
+scissorsBtn.addEventListener("click", () => { playGame("scissors"); });
